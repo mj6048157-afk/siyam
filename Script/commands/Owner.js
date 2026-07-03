597af13d5c028ea1436877e3fe8aa5b20136bae1
mdsiyam01320-tech <mdsiyam01320@gmail.com>
2026-07-04 00:03:30 +0600

    Update Owner.js

--- Script/commands/Owner.js
+++ Script/commands/Owner.js
@@ -5,7 +5,7 @@ module.exports.config = {
   name: "owner",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
+  credits: "𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
   description: "Show Owner Info with styled box & random photo",
   commandCategory: "Information",
   usages: "owner",
@@ -13,38 +13,28 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event }) {
-
-  
-  const info = `
-╔═════════════════════ ✿
-║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
-╠═════════════════════ ✿
-║ 👑 𝗡𝗮𝗺𝗲 : 𝗦𝗛𝗔𝗛𝗔𝗗𝗔𝗧 𝗦𝗔𝗛𝗨
-║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗔𝗛𝗨
-║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
-║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
-║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
-║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗛𝗦𝗖
-║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗞𝗵𝗮𝗴𝗿𝗮𝗰𝗵𝗮𝗿𝗶
-╠═════════════════════ ✿
-║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
-╠═════════════════════ ✿
-║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
-║ fb.com/100001039692046
-║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
-║ m.me/100001039692046
-║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
-║ wa.me/01882333052
-║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 :
-║ t.me/yoursahu
-╚═════════════════════ ✿
-`;
+  const info = `───────────────
+» 👑 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢
+───────────────
+» 👤 𝗡𝗮𝗺𝗲 : 𝗦𝗶𝘆𝗮𝗺 𝗛𝗮𝘀𝗮𝗻
+» 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗶𝘆𝗮𝗺 
+» 🎂 𝗔𝗴𝗲 : 𝟭𝟳+
+» 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
+» 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
+» 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝟭𝟬
+» 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗞𝗶𝘀𝗵𝗼𝗿𝗲𝗴𝗮𝗻𝗷
+───────────────
+» 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
+───────────────
+» 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : fb.com/61591371186179
+» 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 : wa.me/01789138157
+» ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 : +8801789138157
+───────────────
+» 👤 𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍`;
 
   const images = [
-    "https://i.imgur.com/8WBso8x.png",
-    "https://i.imgur.com/0VZu5eY.png",
-    "https://i.imgur.com/bkixgPK.jpeg",
-    "https://i.imgur.com/z6G6L4c.jpeg"
+    "https://i.imgur.com/jM3nKe6.jpeg",
+    "https://i.imgur.com/gQzQ6wK.jpeg"
   ];
 
   const randomImg = images[Math.floor(Math.random() * images.length)];
