d3b50e691059b03f9be06ddc035c8e39baae495d
mdsiyam01320-tech <mdsiyam01320@gmail.com>
2026-07-04 10:47:22 +0600

    Update config.js

--- Script/commands/config.js
+++ Script/commands/config.js
@@ -816,7 +816,7 @@ module.exports.handleReply = async function({ api, event, handleReply }) {
 module.exports.run = async ({ event, api }) => {
   const { threadID, messageID, senderID } = event;
   
-  api.sendMessage("⚙️⚙️─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 Command List ⚙️⚙️"
+  api.sendMessage("⚙️─꯭─⃝‌‌𝗦𝗶𝘆𝗮𝗺 𝗖𝗵𝗮𝘁 𝗕𝗼𝘁 Command List ⚙️"
      + "\n[01] Edit bot bio"
      + "\n[02] Edit bot nicknames"
      + "\n[03] View pending messages"
