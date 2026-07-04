e5d4bd0cdab5df7bd7e963f5b3267bb399a5d256
mdsiyam01320-tech <mdsiyam01320@gmail.com>
2026-07-04 11:53:05 +0600

    Update fork.js

--- Script/commands/fork.js
+++ Script/commands/fork.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "fork",
     version: "1.0.1",
     hasPermssion: 0,
-    credits: "SHAHADAT SAHU",
+    credits: "𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
     description: "Send YouTube channel and GitHub fork link with intro text",
     commandCategory: "other",
     usages: "fork",
@@ -11,12 +11,17 @@ module.exports.config = {
 
 module.exports.run = async function({ api, event }) {
     const message = 
-        "🌟 আসসালামু আলাইকুম সম্মানিত বট ইউজার 🌟\n\n" +
-        "আপনি যদি তৈরি করতে চান, তাহলে চলে যেতে পারেন আমাদের ইউটিউব চ্যানেলে। " +
-        "সেখানে গিয়ে ভিডিও দেখে আপনি খুব সহজে বট তৈরি করে নিতে পারবেন। " +
-        "তাহলে আর দেরি না করে এখনি চলে যান আমাদের ইউটিউব চ্যানেলে।\n\n" +
-        "➤ YouTube Channel: https://youtube.com/@scb-community\n\n" +
-        "🔗 GitHub Fork Link: https://github.com/shahadat-sahu/SHAHADAT-CHAT-BOT";
+        `───────────────\n\n` +
+        `» 🌟 𝗔𝘀𝘀𝗮𝗹𝗮𝗺𝘂 𝗔𝗹𝗮𝗶𝗸𝘂𝗺 🌟\n` +
+        `সম্মানিত বট ইউজার, আপনি যদি নিজের জন্য প্রফেশনাল বট তৈরি করতে চান, তবে আজই আমাদের সাথে যোগাযোগ করুন।\n\n` +
+        `» 👤 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸: https://www.facebook.com/profile.php?id=61591371186179\n` +
+        `» 💬 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽: +𝟴𝟴𝟬𝟭𝟳𝟴𝟵𝟭𝟯𝟴𝟭𝟱𝟳\n\n` +
+        `» 🚀 𝗙𝗲𝗮𝘁𝘂𝗿𝗲𝘀:\n` +
+        `আপনি চাইলে আমাদের 𝗚𝗼𝗱 𝗕𝗼𝘁 𝘃𝟮, 𝘃𝟯 এবং  wilderness 𝘃𝟱 ভার্সন ব্যবহার করতে পারেন, যাতে রয়েছে 𝟲𝟬𝟬+ এরও বেশি প্রিমিয়াম কম্যান্ড সাপোর্ট।\n\n` +
+        `» 🔗 𝗚𝗶𝘁𝗛𝘂𝗯 𝗙𝗼𝗿𝗸 𝗟𝗶𝗻𝗸:\n` +
+        `https://github.com/shahadat-sahu/SHAHADAT-CHAT-BOT\n\n` +
+        `───────────────\n` +
+        `» 👤 𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍`;
 
     return api.sendMessage(message, event.threadID, event.messageID);
 };
